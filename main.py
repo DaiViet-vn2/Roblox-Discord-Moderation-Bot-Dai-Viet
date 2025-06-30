42d0f3702ae4613a9c4571bda1ebccca2a64fc17
DaiViet-vn2 <daivietbtw@gmail.com>
2025-06-30 17:42:10 +0700

    main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import threading
+import server
+import bot
+
+
+def run_flask():
+    server.run()
+
+
+def run_discord():
+    bot.bot.run(bot.TOKEN)
+
+
+if __name__ == "__main__":
+    threading.Thread(target=run_flask).start()
+    run_discord()
